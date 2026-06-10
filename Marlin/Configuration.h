86e0006f43a11a14eaa9d52abec01fabe2b09788
Emanuel <emanuel@local>
2026-06-10 07:59:45 -0300

    Expand X_MIN_POS from -20 to -35 to allow nozzle parking outside bed
    - Previous X_MIN_POS limit was preventing NOZZLE_PARK_POINT at X=-30 (soft endstops blocking)
    - Extended limit to -35 allows filament change and pause to park nozzle completely outside print area
    - Z_PROBE_DEPLOY_X will now use -35 instead of -20 (still within safe movement range)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1971,7 +1971,7 @@
 #define Y_BED_SIZE 235
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
-#define X_MIN_POS -20
+#define X_MIN_POS -35
 #define Y_MIN_POS -7
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
