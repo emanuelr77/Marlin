59e7de5deacdf138f81b6b4f5ab7df16444d6665
Emanuel <emanuel@local>
2026-08-19 19:38:09 -0300

    Revert "Fine-tune retract acceleration: reduce to 750 for smoother dual drive operation"
    This reverts commit f8b545d64913810501a4687cc19b0e1e64f022d9.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1403,7 +1403,7 @@
  *   M204 T    Travel Acceleration
  */
 #define DEFAULT_ACCELERATION           500    // X, Y, Z and E acceleration for printing moves
-#define DEFAULT_RETRACT_ACCELERATION   750    // E acceleration for retracts - balanced for dual drive (smooth but responsive)
+#define DEFAULT_RETRACT_ACCELERATION  1000    // E acceleration for retracts (increased for dual drive)
 #define DEFAULT_TRAVEL_ACCELERATION   1000    // X, Y, Z acceleration for travel (non printing) moves
 
 /**
