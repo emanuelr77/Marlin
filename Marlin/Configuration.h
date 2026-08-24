66948e68ba66eedc1bfe63c611f80c3bccac4420
Emanuel <emanuel@local>
2026-08-24 20:25:50 -0300

    Reduce E_ACCELERATION from 1000 to 500 mm/s² for dual drive
    - Addresses vibration during retraction when STOP is pressed
    - Forum consensus: 500 mm/s² optimal for dual gear extruders
    - Prevents clicking and noise without affecting print speed

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1387,7 +1387,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 1000 }
+#define DEFAULT_MAX_ACCELERATION      { 500, 500, 100, 500 } // E reduced from 1000 to 500 for dual drive (less vibration on retract)
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
